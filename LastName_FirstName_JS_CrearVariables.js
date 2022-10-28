4cc193d178e4bf058b5ef70a1d32229fecb751c3
Carlos Andres Villegas Rendon <54581928+CarlosVillegas1999@users.noreply.github.com>
2022-10-27 20:34:21 -0500

    Add files via upload

---
+++ LastName_FirstName_JS_CrearVariables.js
@@ -0,0 +1,7 @@
+let heigth = 1.80;
+let age = 18;
+
+/*Las variables que se necesitan para determinar si la persona puede subir a la montaña rusa son:
+Altura y Edad, son variables de tipo numero.
+Tambien se necesita una condición, donde "Altura tiene que ser mayor o igual a 1 Metro" y "Edad Mayor a 10 años" 
+*/ 
