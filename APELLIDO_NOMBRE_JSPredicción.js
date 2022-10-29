114a4817e67e92caa5a6fdc432bf5a02c8a83415
Carlos Andres Villegas Rendon <54581928+CarlosVillegas1999@users.noreply.github.com>
2022-10-29 12:59:18 -0500

    Add files via upload

---
+++ APELLIDO_NOMBRE_JSPredicción.js
@@ -0,0 +1,19 @@
+function myBirthYearFunc() {
+    console.log("Nací en" + 1980); //Nací en 1980
+}
+
+
+function myBirthYearFunc(birthYearInput) {
+    console.log("Nací en" + birthYearInput); //Nací en 1980
+}
+
+function add(num1, num2) {
+    console.log("!Sumando números!");  //Indicaria !Sumando números!
+    console.log("num1 es: " + num1);   //Indicaria num1 es: 10
+    console.log("num2 es: " + num2);   //Indicaria num2 es: 20
+    var sum = num1 + num2;
+    console.log(sum);  //Indicaria 30
+}
+
+
+
